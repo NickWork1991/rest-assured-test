4d16de45897f4046f104418eb1aa0ef009511417
NickWork1991 <nikitamekhanikov@coherentsolutions.com>
2025-06-02 13:26:44 +0300

    User tests (#3)
    * New updates
    
    * New updates
    
    ---------
    
    Co-authored-by: NikitaMekhanikov <powermeha91@gmail.com>

--- src/main/java/AuthRequest.java
+++ src/main/java/AuthRequest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AuthRequest {
+import com.google.gson.Gson;
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
+public class AuthRequest {
+    private String username;
+    private String password;
+
 }
--- src/test/java/CreateBookingTest.java
+++ src/test/java/CreateBookingTest.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class CreateBookingTest {
+import io.qameta.allure.*;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("User management")
+@Feature("Create booking")
+public class CreateBookingTest {
+
+    private static final Logger logger= LogManager.getLogger(CreateBookingTest.class);
+    BookingInfo bookingInfo = BookingInfo.builder()
+            .firstname("Nik")
+            .lastname("Mekhanikov")
+            .totalprice(147)
+            .depositpaid(true)
+            .bookingdates(BookingInfo.BookingDates.builder()
+                    .checkin("2025-05-07")
+                    .checkout("2025-05-11")
+                    .build())
+            .additionalneeds("Breakfast")
+            .build();
+
+    @BeforeMethod
+    void beforeCreate(){
+        APIServices apiServices = new APIServices();
+        apiServices.getToken();
+    }
+
+    @Test
+    @Story("Try no create a new booking")
+    @Severity(SeverityLevel.CRITICAL)
+    public void createBookingWithAllFieldsTest(){
+        logger.info("Scenario #1: Verify that can create booking with valid data");
+        Allure.step("Create a new booking with all required fields" + bookingInfo);
+
+        APIServices apiServices = new APIServices();
+        Response response = apiServices.createBooking(bookingInfo);
+
+        Allure.step("Verify that status code is 200");
+        Assert.assertEquals(response.statusCode(),200,"Expected status code - 200");
+    }
 }
