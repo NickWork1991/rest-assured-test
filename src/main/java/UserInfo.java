2417cc58e1f8487e00e67586ffb4dbf86f014063
NikitaMekhanikov <powermeha91@gmail.com>
2025-06-02 13:25:10 +0300

    New updates

--- src/main/java/UserInfo.java
+++ src/main/java/UserInfo.java
@@ -1,14 +1,2 @@
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class UserInfo {
-private int id;
-private String userName;
-private String password;
+package PACKAGE_NAME;public class UserInfo {
 }
